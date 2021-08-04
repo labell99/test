85e2f15bb75df100b01e1ed6bb6391ecc7a56d95
lee abell <lee.abell@finclusionsystems.com>
2021-08-04 11:23:15 +0100

    first commit

--- src/pages/create.js
+++ src/pages/create.js
@@ -2,12 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Layout from 'components/Layout'
 import Content from 'components/Content'
+import { Container } from '@material-ui/core';
+import OrderForm from 'components/form/OrderForm';
 
 function CreatePage({ location }) {
 	const pageTitle = location ? location.pathname.replace(/\//g, '') : ''
 	return (
 		<Layout location={location} title={pageTitle}>
-			<Content />
+		  <Container maxWidth="md" style={{ margin: '1rem auto' }}>
+		    <OrderForm />
+		  </Container>
 			test
 		</Layout>
 	)
