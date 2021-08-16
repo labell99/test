76673b0ce7895bb6546d8ba2ed06aa8b97a00b1a
lee abell <lee.abell@finclusionsystems.com>
2021-08-16 11:19:39 +0100

    first commit

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,37 @@
-import '@trendmicro/react-modal/dist/react-modal.css';
-import Modal from '@trendmicro/react-modal';
+import React from 'react';
+import { Button, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { useSingleModal } from '../contexts/SingleModalContext';
 
-export default Modal;
+const { toggle, close, setContent } = useSingleModal();
+
+const toggleModalWithContent = (cont) => {
+  setContent(cont);
+  toggle();
+};
+
+<Button
+  color="danger"
+  onClick={() =>
+    toggleModalWithContent(() => {
+      return (
+        <>
+          <ModalHeader>Modal title</ModalHeader>
+          <ModalBody>
+            Another Test Another Test Another Test Another Test Another Test Another Test Another Test Another
+            Test Another Test Another Test Another Test Another Test
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={() => close()}>
+              Do Something
+            </Button>{' '}
+            <Button color="secondary" onClick={() => close()}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </>
+);
+    })
+  }
+>
+  Modal with Buttons
+</Button>
