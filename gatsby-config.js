0f1c7a7972a1955582fa3f482d474d6b7648c08f
lee abell <lee.abell@finclusionsystems.com>
2021-08-19 13:02:25 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,20 @@ module.exports = {
 		},
 	},
 	plugins: [
-		{
-			resolve: `gatsby-plugin-material-ui`,
-		},
+	  `gatsby-plugin-react-helmet`,
+	  `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+		 resolve: `gatsby-plugin-material-ui`,
+	     resolve: `gatsby-plugin-manifest`,
+		 options: {
+		   name: `gatsby-starter-default`,
+		   short_name: `starter`,
+		   start_url: `/`,
+		   background_color: `#663399`,
+		   theme_color: `#663399`,
+		   display: `minimal-ui`,
+         },
+	  },
 	],
 }
